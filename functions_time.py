5f498098d4fbbdf54d3668cdadb2a2c453f5b041
Lindy Castellaw <lindy.c.castellaw@gmail.com>
2022-08-12 16:58:55 -0500

    update

--- functions_time.py
+++ functions_time.py
@@ -3,17 +3,17 @@ import os
 import matplotlib as plt
 
 def clean_store(df):
-   df.sale_date = pd.to_datetime('Jan:1:1970', format='%b:%d:%Y')
-   df = df.set_index('sale_date').sort_index() #make date the index
+   df.sale_date = pd.to_datetime(df.sale_date, infer_datetime_format=True)
+   df.set_index('sale_date', inplace=True) #make date the index
    df['weekday'] = df.index.day_name()
    df['month'] = df.index.month_name()
    df['sales_total'] = df['sale_amount'] * df['item_price']
    return df
 
 def clean_power(df):
-    df.Date = pd.to_datetime('Jan:1:1970', format='%b:%d:%Y')
-    df = df.set_index('Date').sort_index()
+    df.Date = pd.to_datetime(df.Date)
+    df.set_index('Date', inplace=True)
+    df['day_of_week'] = df.index.strftime('%A')
     df['month'] = df.index.month_name()
-    df['year'] = df.index.year
     df = df.fillna(0)
     return df
